5d33021c3ab9ae8ac315959702a9e7f332e3e12a
ujum <vladzislavjum@outlook.com>
2021-08-23 10:38:08 +0300

    feat: add app graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,17 +1,31 @@
 package app
 
 import (
+	"github.com/kataras/iris/v12"
 	"github.com/ujum/dictap/internal/app/config"
 	"github.com/ujum/dictap/internal/app/server"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func Run(configFilePath string) {
 	appConfig, err := config.New(configFilePath)
 	if err != nil {
-		log.Printf("Can't override default config. Using default. Reason: %v", err)
+		log.Printf("can't override default config. Using default. Reason: %v", err)
 	}
-	if err := server.Start(appConfig); err != nil {
+	srv, err := server.Start(appConfig)
+	if err != nil {
 		log.Fatalf("%v", err)
+		return
 	}
+	listenOSStopSignals(srv)
+}
+
+func listenOSStopSignals(srv *iris.Application) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGSTOP)
+	log.Printf("system call: %+v", <-signalChan)
+	server.Stop(srv)
 }
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,14 +1,16 @@
 package server
 
 import (
+	"context"
 	"github.com/kataras/iris/v12"
 	"github.com/ujum/dictap/internal/app/config"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
 )
 
-func Start(cfg *config.Config) error {
+func Start(cfg *config.Config) (*iris.Application, error) {
 	app := iris.New()
 	route(app)
 	srv := &http.Server{
@@ -18,11 +20,24 @@ func Start(cfg *config.Config) error {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	err := app.Run(iris.Server(srv))
-	if err != nil {
-		return err
+	go func() {
+		if err := app.Run(iris.Server(srv)); err != nil {
+			log.Fatalf("%v", err)
+		}
+	}()
+
+	return app, nil
+}
+
+func Stop(app *iris.Application) {
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+	if err := app.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("server shutdown Failed: %v", err)
 	}
-	return nil
+	log.Printf("http: Server closed")
 }
 
 func route(app *iris.Application) {
