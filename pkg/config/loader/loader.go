19be34fcbbe17e200244c334754ba7f38136f135
ujum <vladzislavjum@outlook.com>
2021-08-24 17:41:38 +0300

    fix(config): load several independent settings

--- pkg/config/loader/loader.go
+++ pkg/config/loader/loader.go
@@ -24,14 +24,15 @@ type LoadSettings struct {
 // Load config from files and system env to app config struct.
 // Use 'APP_ENV' sys env key to denote app profile config.
 func Load(configStruct interface{}, loadSettings *LoadSettings) error {
-	viper.SetConfigType(loadSettings.ConfigType)
-	viper.AddConfigPath(loadSettings.ConfigDir)
-	mergeCommonConfig(loadSettings)
-	mergeAppEnvConfig(loadSettings)
+	v := viper.New()
+	v.SetConfigType(loadSettings.ConfigType)
+	v.AddConfigPath(loadSettings.ConfigDir)
+	mergeCommonConfig(v, loadSettings)
+	mergeAppEnvConfig(v, loadSettings)
 	if loadSettings.LoadSysEnv {
-		mergeSystemEnvConfig(loadSettings.EnvPrefix)
+		mergeSystemEnvConfig(v, loadSettings.EnvPrefix)
 	}
-	if err := viper.Unmarshal(configStruct); err != nil {
+	if err := v.Unmarshal(configStruct); err != nil {
 		log.Fatalf("unable to decode into struct, %v", err)
 		return err
 	}
@@ -39,29 +40,29 @@ func Load(configStruct interface{}, loadSettings *LoadSettings) error {
 }
 
 // merge common(base) config file to config struct
-func mergeCommonConfig(loadSettings *LoadSettings) {
+func mergeCommonConfig(v *viper.Viper, loadSettings *LoadSettings) {
 	const baseConfigName = "base"
-	viper.SetConfigName(resolveFileName(loadSettings.FileNamePrefix, baseConfigName))
-	if err := viper.MergeInConfig(); err != nil {
+	v.SetConfigName(resolveFileName(loadSettings.FileNamePrefix, baseConfigName))
+	if err := v.MergeInConfig(); err != nil {
 		log.Printf("%v", err)
 		return
 	}
-	log.Printf("loaded %s config (%s)", baseConfigName, viper.ConfigFileUsed())
+	log.Printf("loaded %s config (%s)", baseConfigName, v.ConfigFileUsed())
 }
 
 // merge app profile config file to config struct
-func mergeAppEnvConfig(loadSettings *LoadSettings) {
+func mergeAppEnvConfig(v *viper.Viper, loadSettings *LoadSettings) {
 	var envKey = applicationEnv
 	if loadSettings.EnvPrefix != "" {
 		envKey = strings.ToUpper(loadSettings.EnvPrefix) + "_" + applicationEnv
 	}
 	profile := os.Getenv(envKey)
 	if profile == "" {
-		profile = viper.GetString("app.profile")
+		profile = v.GetString("app.profile")
 	}
 	if profile != "" {
-		viper.SetConfigName(resolveFileName(loadSettings.FileNamePrefix, profile))
-		if err := viper.MergeInConfig(); err != nil {
+		v.SetConfigName(resolveFileName(loadSettings.FileNamePrefix, profile))
+		if err := v.MergeInConfig(); err != nil {
 			if err, ok := err.(viper.ConfigFileNotFoundError); ok {
 				log.Printf("config for %s profile not found, %v", profile, err)
 			} else {
@@ -69,16 +70,16 @@ func mergeAppEnvConfig(loadSettings *LoadSettings) {
 			}
 			return
 		}
-		log.Printf("loaded %v profile config (%s)", profile, viper.ConfigFileUsed())
+		log.Printf("loaded %v profile config (%s)", profile, v.ConfigFileUsed())
 	}
 }
 
 // merge system environment variables to config struct
-func mergeSystemEnvConfig(envPrefix string) {
-	viper.SetEnvPrefix(envPrefix)
-	viper.AllowEmptyEnv(false)
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+func mergeSystemEnvConfig(v *viper.Viper, envPrefix string) {
+	v.SetEnvPrefix(envPrefix)
+	v.AllowEmptyEnv(false)
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
 }
 
 func resolveFileName(filePrefix, name string) string {
